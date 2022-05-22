4f26dc4cae43525e80070d51ddd703b1bf4e2d08
Mesut Neozil <justinneoyunhao@gmail.com>
2022-05-22 18:53:18 +0800

    learnt about word similarities

--- server/test.py
+++ server/test.py
@@ -1,7 +1,11 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
-text = 'ur mum gay'
-test = nlp (text)
-for token in test:
-    print(token)
+nlp = spacy.load("en_core_web_md")  
+doc1 = nlp("I like salty fries and hamburgers.")
+doc2 = nlp("Fast food tastes very good.")
+print(doc1, "<->", doc2, doc1.similarity(doc2))
+
+
+french_fries = doc1[2:4]
+burgers = doc1[5]
+print(french_fries, "<->", burgers, french_fries.similarity(burgers))
 
