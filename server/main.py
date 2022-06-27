afb9d2318d2edd9975b7605fed3840937c19c51d
Lanreath <tyfei00@gmail.com>
2022-06-27 15:03:54 +0800

    Init api methods

--- server/main.py
+++ server/main.py
@@ -1,22 +1,46 @@
+import crochet
+crochet.setup()
 from typing import Optional, Union
+from unittest import signals
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from scrapy.crawler import CrawlerRunner
+from scrapy.signalmanager import dispatcher
+from listings.spiders.shopee import ShopeeSpider
+
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    isOffer: Optional[bool] = None
+origins = []
+
+crawl_runner = CrawlerRunner()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/listings")
+async def find_product(query: str):
+    return 
 
-@app.put("items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/sentiment/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return
+
+def scrape_with_crochet():
+    dispatcher.connect(_crawler_result, signal=signals.item_scraped)
+
+    eventual = crawl_runner.crawl(ShopeeSpider)
+    return eventual
+
+def _crawler_result(item, response, spider):
+    return
