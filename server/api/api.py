845f04ed4f71323481ca3bbf1f9f6463e6d01e1b
Lanreath <tyfei00@gmail.com>
2022-07-10 23:04:04 +0800

    Added endpoints for queries, yet to do listings and reviews

--- server/api/api.py
+++ server/api/api.py
@@ -3,13 +3,17 @@ import crochet
 crochet.setup()
 from typing import Optional, Union
 from scrapy import signals
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from scrapy.crawler import CrawlerRunner
 from scrapy.signalmanager import dispatcher
+from sqlalchemy.orm import Session
 from listings.spiders.shopee import ShopeeSpider
+from db import models, database, crud
+from . import schemas
 
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
@@ -42,39 +46,32 @@ async def find_product(query: str):
     data = await scrape_with_crochet(search= query)
     return data
 
-@app.get("/sentiment/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return
-
 # Dependency
 def get_db():
-    db = SessionLocal()
+    db = database.SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+@app.post("/query/", response_model=schemas.Query)
+def create_user(query: schemas.QueryCreate, db: Session = Depends(get_db)):
+    return crud.create_query(db, query=query)
 
 
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+@app.get("/query/", response_model=schemas.User)
+def read_query(query: str, db: Session = Depends(get_db)):
+    db_query = crud.get_query_by_term(db, term=query)
+    if db_query is None:
+        raise HTTPException(status_code=400, detail="Not scraped")
+    return db_query
 
+@app.get("/query/{query_id}", response_model=schemas.Query)
+def read_query_by_id(query_id: int, db: Session = Depends(get_db)):
+    db_query = crud.get_query(db, query_id==query_id)
+    return db_query
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
 
 
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
