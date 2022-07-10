8174063580eb8a1d5c7090234ddda9c5739fb5e7
Lanreath <tyfei00@gmail.com>
2022-07-10 23:04:59 +0800

    Minor changes

--- server/api/api.py
+++ server/api/api.py
@@ -71,17 +71,3 @@ def read_query(query: str, db: Session = Depends(get_db)):
 def read_query_by_id(query_id: int, db: Session = Depends(get_db)):
     db_query = crud.get_query(db, query_id==query_id)
     return db_query
-
-
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
